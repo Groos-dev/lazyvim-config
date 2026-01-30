e56c5309036b41b5a30a97a04130e0a43b9569a7
groos <gegu926@gmail.com>
2026-01-30 13:19:14 +0800

    fix(config): disable spell checking and markdown linting
    - Disable LazyVim default spell checking in autocmds
    - Add markdown plugin config to disable markdownlint warnings
    - Resolves MD013 line-length and other formatting warnings

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+}
